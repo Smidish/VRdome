5e0d78cf64a0a6fe3e727a052e4cd892de82d260
Smilla Hinterreiter <s.hinterreiter@gmx.de>
2020-06-24 11:27:24 +0200

    fixed animation / shader

--- MobilVR/Assets/Scripts/InteractionGUI.cs
+++ MobilVR/Assets/Scripts/InteractionGUI.cs
@@ -8,13 +8,21 @@ public class InteractionGUI : MonoBehaviour {
 
     public GameObject SelectedObject;
     public GameObject TopicCanvas;
+    public Material dissolveMat;
 
-    private float timer;
-    public float gazeTime = 2f;
+    private float timer = 1f;
+    public float gazeTime = 3f;
     private bool gazedAt;
-    private float sizze = 1.5F;
+    private float sizze = 1F;
 
+    private float dissolveInt = 0.9f;
+    private Vector3 defaultScale;
 
+    void Start()
+    {
+        Transform child = transform.GetChild(0);
+        defaultScale = child.localScale;
+    }
     void Update()
     {
         if (gazedAt)
@@ -22,12 +30,13 @@ public class InteractionGUI : MonoBehaviour {
             timer += Time.deltaTime;
             Transform child = transform.GetChild(0);
             Vector3 newScale = new Vector3(timer / gazeTime* sizze, timer / gazeTime* sizze, timer / gazeTime* sizze);
-
+                       
             child.localScale = newScale;
+            dissolveMat.SetFloat("TimeActivation", gazeTime * dissolveInt);
 
-            if(timer >= gazeTime)
+            if (timer >= gazeTime)
             {
-                timer = 0f;
+                timer = 1f;
                 selectBubble();
                 //ExecuteEvents.Execute(gameObject, new PointerEventData(EventSystem.current), ExecuteEvents.pointerDownHandler);
             }
@@ -37,14 +46,16 @@ public class InteractionGUI : MonoBehaviour {
     public void BubbleIn()
     {
         gazedAt = true;
-        //trigger loading animation (2sec)
-
     }
 
     public void BubbleOut()
     {
         gazedAt = false;
-
+        timer = 1f;
+        Transform child = transform.GetChild(0);
+        child.localScale = defaultScale;
+        Debug.Log(defaultScale);
+        SelectedObject.SetActive(true);
 
     }
 
@@ -52,15 +63,13 @@ public class InteractionGUI : MonoBehaviour {
     {
         Debug.Log("Pointer Down");
        // Destroy(SelectedObject);
-
-
     }
 
     private void selectBubble()
     {
-
-        TopicCanvas.SetActive(true);
         SelectedObject.SetActive(false);
+          TopicCanvas.SetActive(true);
+        
     //Destroy(SelectedObject);
 
     }
