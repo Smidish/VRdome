915890918353ef5fc5ebbdfce901200785931881
Smilla Hinterreiter <s.hinterreiter@gmx.de>
2020-06-24 09:06:53 +0200

    init shader bug

---
+++ MobilVR/Assets/Scripts/ChangeColor.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeColor : MonoBehaviour
+{
+
+    public Material dissolveMat;
+
+    public void DissolveIn() {
+        Debug.Log("hi");
+        dissolveMat.SetFloat("TimeActivation", 0);
+        
+    }
+
+    public void DissolveOut()
+    {
+        Debug.Log("hi");
+        dissolveMat.SetFloat("TimeActivation", 1);
+        
+    }
+
+
+    public void Red()
+    {
+        GetComponent<Renderer>().material.SetColor("_Color", Color.red);
+    }
+
+    public void Blue()
+    {
+        GetComponent<Renderer>().material.SetColor("_Color", Color.blue);
+    }
+
+    public void Black()
+    {
+        GetComponent<Renderer>().material.SetColor("_Color", Color.black);
+    }
+}
---
+++ MobilVR/Assets/Scripts/InteractionGUI.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class InteractionGUI : MonoBehaviour { 
+
+
+    public GameObject SelectedObject;
+    public GameObject TopicCanvas;
+
+    private float timer;
+    public float gazeTime = 2f;
+    private bool gazedAt;
+    private float sizze = 1.5F;
+
+
+    void Update()
+    {
+        if (gazedAt)
+        {
+            timer += Time.deltaTime;
+            Transform child = transform.GetChild(0);
+            Vector3 newScale = new Vector3(timer / gazeTime* sizze, timer / gazeTime* sizze, timer / gazeTime* sizze);
+
+            child.localScale = newScale;
+
+            if(timer >= gazeTime)
+            {
+                timer = 0f;
+                selectBubble();
+                //ExecuteEvents.Execute(gameObject, new PointerEventData(EventSystem.current), ExecuteEvents.pointerDownHandler);
+            }
+        }
+    }
+
+    public void BubbleIn()
+    {
+        gazedAt = true;
+        //trigger loading animation (2sec)
+
+    }
+
+    public void BubbleOut()
+    {
+        gazedAt = false;
+
+
+    }
+
+    public void PointerDown()
+    {
+        Debug.Log("Pointer Down");
+       // Destroy(SelectedObject);
+
+
+    }
+
+    private void selectBubble()
+    {
+
+        TopicCanvas.SetActive(true);
+        SelectedObject.SetActive(false);
+    //Destroy(SelectedObject);
+
+    }
+}
